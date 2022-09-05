86744030bd16f67b654ebbf3f8b3a41502541670
Guilherme Cunha <guilherme.cunha@VET-120052.ad.vetorial.com>
2022-09-05 14:55:22 -0300

    Aula 2

--- Aula1.py
+++ Aula1.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv("Dados aula 1.csv", encoding= "UTF-8", sep=";")
+dfexcel = pd.read_excel("Dados Excel.xlsx", sheet_name=1)
+dfcsv = pd.read_csv("Dados aula 1.csv", encoding = "UTF-8", sep = ";", usecols=["AddressID", "AddressLine1"], nrows=200)
 
-print(df.head())
+arquivo_excel = pd.ExcelFile("Dados Excel.xlsx")
+print(arquivo_excel.sheet_names)
+aba1 = arquivo_excel.parse('Dados Excel')
+aba2 = arquivo_excel.parse('segunda')
+print(aba1)
+print(aba2)
+#print(dfexcel.head())
+#print(dfcsv.head())
